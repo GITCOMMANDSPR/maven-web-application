ad55507ad1a68837092436d83c61c75d34619a30
KOLLI PAVAN <102366432+kollipavan@users.noreply.github.com>
2022-07-01 17:55:25 +0530

    Update 1terraformscript.tf.vb

--- 1terraformscript.tf.vb
+++ 1terraformscript.tf.vb
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = " ap-south-1"
+  region = "ap-south-1"
   access_key = "AKIA3GKX7UKIVC32PQ4T"
   secret_key = "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"
 }
 resource "aws_instance" "AWSEC2Instance" {
-  ami = "          "
+  ami = "ami-05c8ca4485f8b138a"
   instance_type = "t2.micro"
-  security_groups = ["         "]
-  key_name = "         "
+  security_groups = ["launch-wizard-1"]
+  key_name = "demokey"
   tags = {
     Name = " RedHat server by terraform"
   }
